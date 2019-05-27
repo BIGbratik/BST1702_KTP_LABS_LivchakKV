fe0d43148c2defd54b3c1003c6337ccf5ce185b3
BIGbratik <prayDEATH@mail.ru>
2019-05-27 17:29:51 +0300

    Delete Ex1.py

--- Ex1.py
+++
@@ -1,9 +0,0 @@
-a=float(input("Введите a (желательно не 0 !!!) ->\t"))
-b=float(input("Введите b ->\t"))
-c=float(input("Введите c ->\t"))
-d=float(input("Введите d ->\t"))
-f=float(input("Введите f ->\t"))
-if a==0:
-    print("Ошибочка:\ta = 0")
-else:
-    print(abs(a-b*c*d**3+(c**5-a**2)/a+f**3*(a-213)))
